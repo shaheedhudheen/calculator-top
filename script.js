640d542e52465c2fce6f7812a88b53a9bdcff100
Ahammed Shaheedhudheen <shaheedhudheenpambalath@gmail.com>
2022-08-12 00:34:10 +0530

    Fixed some bugs and changed some UI elements

--- script.js
+++ script.js
@@ -1,97 +1,129 @@
-let firstNumber = "";
-let secondNumber = "";
-let currentOperator = null;
+let currentOperand = "";
+let previousOperand = "";
+let operator = null;
 let result = "";
-
+let haveDot = false;
 const numberButtons = document.querySelectorAll(".number");
 const operatorsButtons = document.querySelectorAll(".opr");
 const equalButton = document.querySelector(".equal");
 const clearButton = document.querySelector(".cls");
 const deleteButton = document.querySelector(".del");
 
-const upperScreen = document.querySelector(".upperScreen");
-const lowerScreen = document.querySelector(".lowerScreen");
+const operationsScreen = document.querySelector(".operationsScreen");
+const resultScreen = document.querySelector(".resultScreen");
 
-lowerScreen.textContent = "";
 
-function clearScreen() {
-  lowerScreen.textContent = null;
+
+//clear
+function clear() {
+  currentOperand = "";
+  previousOperand = "";
+  operator = undefined;
+  haveDot = false;
 }
 
-function appendLowerScreen(value) {
-  lowerScreen.textContent += value;
+//delete
+function deleteNum() {
+  currentOperand = currentOperand.toString().slice(0, -1);
+}
+//append number
+function appendNumber(number) {
+  if (number === "." && !haveDot) {
+    haveDot = true;
+  } else if (number === "." && haveDot) {
+    return;
+  }
+  currentOperand += number;
 }
 
-//selecting operator button
-function selectCurrentOperator(value) {
-  firstNumber = lowerScreen.textContent;
-  currentOperator = value;
-  clearScreen();
+//choose operation
+function chooseOperator(value) {
+  if (currentOperand === "") return;
+  if (previousOperand !== "") {
+    operate();
+  }
+  operator = value;
+  previousOperand = currentOperand;
+  currentOperand = "";
 }
 
-//numbers button event
-numberButtons.forEach((button) => {
-  button.addEventListener("click", () => appendLowerScreen(button.value));
-});
+//compute
 
-//operator selection event
-operatorsButtons.forEach((operator) => {
-  operator.addEventListener("click", () =>
-    selectCurrentOperator(operator.value)
-  );
-});
+function operate() {
+  let num1 = Number(currentOperand);
+  let num2 = Number(previousOperand);
 
-//equal to button event
-equalButton.addEventListener("click", (e) => {
-  secondNumber = lowerScreen.textContent;
-  clearScreen();
-  operate(firstNumber, secondNumber, currentOperator);
-});
-
-clearButton.addEventListener("click", () => {
-  firstNumber = "";
-  secondNumber = "";
-  currentOperator = "";
-
-  lowerScreen.textContent = "";
-  upperScreen.textContent = "";
-});
-
-//operations function
-
-let add = (num1 = 0, num2 = 0) => num1 + num2;
-
-let substract = (num1 = 0, num2 = 0) => num1 - num2;
-
-let multiply = (num1 = 0, num2 = 0) => num1 * num2;
-
-let divide = (num1 = 0, num2 = 1) => num1 / num2;
-
-function operate(firstNumber, secondNumber, currentOperator) {
-  let operator = currentOperator;
-  let num1 = Number(firstNumber);
-  let num2 = Number(secondNumber);
+  if (isNaN(currentOperand) || isNaN(previousOperand)) return;
+  console.log(num1);
+  console.log(num2);
 
   let result = 0;
   switch (operator) {
     case "+":
-      result = add(num1, num2);
+      result = num1 + num2;
       break;
     case "-":
-      result = substract(num1, num2);
+      result = num1 - num2;
       break;
     case "*":
     case "x":
     case "X":
-      result = multiply(num1, num2);
+      result = num1 * num2;
       break;
     case "/":
-      result = divide(num1, num2);
+      result = num1 / num2;
       break;
     default:
-      result = "enter a valid operator";
-      break;
+      return;
   }
   console.log(result);
-  upperScreen.textContent = result;
+  currentOperand = result;
+  operator = undefined;
+  previousOperand = "";
 }
+
+//update Display
+function updateDisplay() {
+  resultScreen.textContent = currentOperand.toString();
+    if(operator != null){ 
+        operationsScreen.textContent = `${previousOperand}${operator}`
+    } else{
+        operationsScreen.textContent = ''
+    }
+}
+
+//numbers button event
+numberButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    appendNumber(btn.value);
+    updateDisplay();
+  });
+});
+
+//operator button event
+operatorsButtons.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    chooseOperator(operator.value);
+    updateDisplay();
+  });
+});
+
+//equal to button event
+equalButton.addEventListener("click", (e) => {
+  operate();
+  updateDisplay();
+});
+
+//clear button event
+clearButton.addEventListener("click", () => {
+  clear();
+  updateDisplay();
+});
+
+//delete buttton event
+deleteButton.addEventListener('click', ()=>{
+    deleteNum();
+    updateDisplay();
+})
+
+
